f30d974bf092728b62bf5bfa336bcd3b46346b3e
Canteko <canteko@gmail.com>
2017-09-04 12:32:23 +0200

    UCO

--- Fracaso2016/eleccion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PartidoPolitico, Voto, MesaElectoral, Circunscripcion, Resultado
-
-admin.site.register(PartidoPolitico)
-admin.site.register(Voto)
-admin.site.register(MesaElectoral)
-admin.site.register(Circunscripcion)
-admin.site.register(Resultado)
